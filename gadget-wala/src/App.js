9c12f41aa3d14252192e547835dc8352d4e1b2c2
sangmmagariaravind123 <sangmmagariaravind123@gmail.com>
2023-11-24 23:07:40 +0530

    v-nav

--- gadget-wala/src/App.js
+++ gadget-wala/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar1 from './Components/Navbar1/Navbar1';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
     <div className="App">
