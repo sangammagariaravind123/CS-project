ab747fbe975f3f000419b2754cb149e26d8f715b
sangmmagariaravind123 <sangmmagariaravind123@gmail.com>
2023-11-24 23:05:25 +0530

    .

---
+++ gadget-wala/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Navbar1 from './Components/Navbar1/Navbar1';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar1 />
+    </div>
+  );
+}
+
+export default App;
